b25fa563ea154fa1b6637d718aa9a7b16ee2d066
Wallace <wallacefcardoso@gmail.com>
2022-07-21 15:34:46 -0300

    tidy up work scheduling controller

--- services/Twenty2ScheduleSolution/Twenty2.Schedule.Api/Controllers/WorkSchedulingApi.cs
+++ services/Twenty2ScheduleSolution/Twenty2.Schedule.Api/Controllers/WorkSchedulingApi.cs
@@ -2,16 +2,20 @@
 
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Twenty2.Schedule.Api.Repositories;
 using Twenty2.Schedule.Shared.Api;
 using Twenty2.Schedule.Shared.Dto;
 
 [ApiController]
-[Route( "work-scheduling" )]
+[Route( "api/work-scheduling" )]
 public class WorkSchedulingApi : IWorkSchedulingApi
 {
-    public WorkSchedulingApi()
+    private readonly IWorkerAvailabilityRepository workerAvailabilityRepository;
+
+    public WorkSchedulingApi(
+        IWorkerAvailabilityRepository workerAvailabilityRepository )
     {
-        
+        this.workerAvailabilityRepository = workerAvailabilityRepository;
     }
 
     [HttpGet( "get-worker-availability-list" )]
